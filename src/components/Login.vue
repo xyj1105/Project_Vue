7c943f1d53d1ee97fc28886c77155de5c7a8b148
xyj1105 <2450926451@qq.com>
2020-05-29 17:24:01 +0800

    添加样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -48,3 +48,34 @@ export default {
   }
 }
 </script>
+
+<style>
+  #poster {
+    background:url("../assets/timg.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 200px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+</style>
