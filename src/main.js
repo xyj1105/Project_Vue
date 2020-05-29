ea1c9508c864b396dbd10cec75cac2361b90bcc1
xyj1105 <2450926451@qq.com>
2020-05-29 17:12:47 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 设置方向代理， 前端请求默认发送到 http://localhost:8443/api
+var axios = require('axios')
+
+axios.defaults.baseURL = 'http://localhost:8443/api'
+
+Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import AppIndex from '@/components/home/Appindex'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
+    }
+  ]
+})
